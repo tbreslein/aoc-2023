ccaf4d734ca67a6d9ff2313ee13a9471d3367c08
Tommy Breslein <tommy.breslein@protonmail.com>
2023-11-29 16:04:22 +0100

    init commit

---
+++ flake.nix
@@ -0,0 +1,16 @@
+{
+  description = "A devShell example";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = import nixpkgs { inherit system; };
+      in with pkgs; {
+        devShells.default = mkShell { buildInputs = [ uiua zig zls ]; };
+      });
+}
+
