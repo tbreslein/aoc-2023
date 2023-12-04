f655fed6c4e7e2bdba6260a27a3e7d74e59f0c81
Tommy Breslein <tommy.breslein@protonmail.com>
2023-12-04 14:37:39 +0100

    damn, uiua in nixpkgs was pretty out of date

--- flake.nix
+++ flake.nix
@@ -4,13 +4,20 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
+    uiua-flake = {
+      url = "github:uiua-lang/uiua";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, flake-utils, ... }:
+  outputs = { self, nixpkgs, flake-utils, uiua-flake, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let pkgs = import nixpkgs { inherit system; };
       in with pkgs; {
-        devShells.default = mkShell { buildInputs = [ uiua zig zls ]; };
+        devShells.default = mkShell {
+          buildInputs =
+            [ uiua-flake.outputs.packages."${system}".default zig zls ];
+        };
       });
 }
 
